b9d63cf0892048a363133bb6e5331ecb9b0da9d0
Patricia Macedo <59732005+patriciamacedo@users.noreply.github.com>
2026-02-26 10:00:48 +0000

    remove comments
    Refactor shipping label method to avoid direct modification of Address.

--- src/main/java/Ex3/Order.java
+++ src/main/java/Ex3/Order.java
@@ -9,7 +9,6 @@ class Order {
         String city = customer.getAddress().getCity();
         System.out.println("Shipping to: " + city);
 
-        // Bad: Order changing internals of Address directly via chain
         customer.getAddress().setCity(city.trim().toUpperCase());
     }
 
